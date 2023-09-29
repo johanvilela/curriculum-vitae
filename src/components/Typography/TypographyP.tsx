f7d7b8891fa21579fff84b3d7f3c39e32ddf2207
Johan Vilela <johanvilela@hotmail.com>
2023-09-28 21:20:15 -0300

    chore: justify paragraph text

--- src/components/Typography/TypographyP.tsx
+++ src/components/Typography/TypographyP.tsx
@@ -1,5 +1,9 @@
 import { ReactNode } from "react";
 
 export function TypographyP({ children }: { children: ReactNode }) {
-  return <p className="leading-7 [&:not(:first-child)]:mt-6">{children}</p>;
+  return (
+    <p className="text-justify leading-7 [&:not(:first-child)]:mt-6">
+      {children}
+    </p>
+  );
 }
