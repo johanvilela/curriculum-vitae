5ce223fc5fc82b11b432b3b7680ec6f0bdcda0f7
Johan Vilela <johanvilela@hotmail.com>
2023-09-28 21:21:12 -0300

    chore: add name on PageHeader

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,11 +1,17 @@
 import { ModeToggle } from "@/components/ModeToggle";
 import { Avatar } from "@/components/Avatar";
+import { TypographyH1 } from "../Typography/TypographyH1";
 
 export function PageHeader() {
   return (
     <div className="mt-3 flex justify-between">
-      <div>
-        <Avatar />
+      <div className="flex">
+        <div>
+          <Avatar />
+        </div>
+        <div className="ml-2 self-center">
+          <TypographyH1>Johan Vilela</TypographyH1>
+        </div>
       </div>
       <div className="hide-on-print">
         <ModeToggle />
