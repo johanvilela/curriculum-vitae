2a31a01ecc58935156bca8312acc1afd17e63073
Johan Vilela <johanvilela@hotmail.com>
2023-09-28 21:20:41 -0300

    chore: change TypographyH1 size

--- src/components/Typography/TypographyH1.tsx
+++ src/components/Typography/TypographyH1.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function TypographyH1({ children }: { children: ReactNode }) {
   return (
-    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight">
       {children}
     </h1>
   );
